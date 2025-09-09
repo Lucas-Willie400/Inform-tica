6d57723e89a8a29ceba19e25c02717f9e75f212a
Lucas-Willie400 <lucas.sousa497@aluno.ce.gov.br>
2025-09-09 11:07:00 -0300

    Tarefa de funções.
    Tarefa de funções do python, eu e a Luna fizemos até a questão 6.

---
+++ funções.py
@@ -0,0 +1,47 @@
+#Questão 1:
+# def saudacao(nome):
+#     return(f"Oi {nome}, seja bem-vindo(a)")
+# print(saudacao("Luna"))
+
+# Questão 2:
+# def numero(a):
+#     if a % 2 == 0:
+#         return("número par")
+#     else:
+#         return("número ímpar")
+# print(numero(int(input("digite um número: "))))
+
+# Questão 3:
+# def comparar(a,b):
+#     if a>b:
+#         return(f"{a} é maior")
+#     elif b>a:
+#         return(f"{b} é maior")
+#     elif a==b:
+#         return(f"{a} é igual a {b}")
+# print(comparar(int(input("digite um número: ")), int(input("digite outro número: "))))
+
+#Questão 4:
+# n = int(input("Digite um número: "))
+# for i in range(1, 11):
+#     print(n*i)
+
+# Questão 5
+# n =10
+# while n >=1:
+#     n-=1
+#     if n ==0:
+#         print(0)
+#         print("Explosão.")
+#     else:
+#         print(n)
+
+#Questão 6:
+# nota = int(input("Digite a nota: "))
+# n = 0
+# def aluno(notasquantidade):
+#     for i in notasquantidade:
+#         n += nota
+# print([1, 2, 3, 4])
+# print(nota/i)
+    
